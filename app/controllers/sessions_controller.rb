a19cb730018e313df3263ae70367977d27b21ae1
dnestoff & Jack Noble <pair+dnestoff.winged115@devbootcamp.com>
2016-10-03 14:58:33 -0500

    Log in works, errors champange popping

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def new
+    render :new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user
+      if @user.authenticate(params[:password])
+        session[:user_id] = @user.id
+        redirect_to :root
+      else
+        @errors = ["Wrong email or password"]
+        render :new
+      end
+    else
+      @errors = ["Wrong email or password"]
+      render :new
+    end
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,8 @@ Rails.application.routes.draw do
   get '/users/:id' => 'users#show'
   post '/users' => 'users#create'
 
-
+  get '/sessions/new' => 'sessions#new'
+  post '/sessions' => 'sessions#create'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
