7bd1a27cd7f291e5124f339f92753fc3bea1685a
dnestoff & Jack Noble <pair+dnestoff.winged115@devbootcamp.com>
2016-10-03 15:36:39 -0500

    Add form that can be manipulated for new and edit

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,4 +8,29 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
     render :show
   end
+
+  def new
+    @question = Question.new
+    render :new
+  end
+
+  def create
+    @question = Question.new(question_params)
+
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:title, :question_text)
+  end
 end
