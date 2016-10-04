3b3b65b49aedcfd9cbe5b12cf95393f8ab681dbc
dnestoff & Jack Noble <pair+dnestoff.winged115@devbootcamp.com>
2016-10-03 19:16:40 -0500

    Look for AJAX links

---
+++ vendor/assets/javascripts/lightning_ajax.js
@@ -0,0 +1,5 @@
+// http://guides.rubyonrails.org/working_with_javascript_in_rails.html
+
+//https://coderwall.com/p/kqb3xq/rails-4-how-to-partials-ajax-dead-easy
+
+//remote: true is one of the most important features of allowing AJAX to work.
